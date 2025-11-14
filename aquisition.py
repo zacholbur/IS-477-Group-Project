2f3bc355b78dd089a29c1df98085f15ca03eab1b
djfeder2 <123120069+djfeder2@users.noreply.github.com>
2025-11-14 15:31:10 -0600

    Update aquisition.py

--- aquisition.py
+++ aquisition.py
@@ -2,10 +2,9 @@ import kaggle
 import pandas as pd
 import os
 
+df_spotify = pd.read_csv("hf://datasets/maharshipandya/spotify-tracks-dataset/dataset.csv")
+df_spotify.to_csv("SpotifySubset.csv")
+
 kaggle.api.authenticate()
-
-kaggle.api.dataset_download_files('paradisejoy/top-hits-spotify-from-20002019', path='.', unzip=True)
-os.rename("songs_normalize.csv","SpotifySubset.csv")
-
 kaggle.api.dataset_download_files("sansastark/subset-of-the-million-song-dataset", path='.', unzip=True)
 os.remove("billboard_rank.csv")
